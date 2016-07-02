e80c90c382f470509fbbc0956cd5f6a72964d95d
Will Durkin <willdurkin@gmail.com>
2016-07-02 17:30:50 -0400

    got divs to load from firebase IDs

---
+++ js/app3.1.js
@@ -0,0 +1,142 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // loadPlayer();
+});
+
+// Input fields
+$('#urlInput').click(function() {
+  console.log('slide');
+  $('.slider').slideDown(500).css('display', 'flex');
+});
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD33LPylUvbdB7cq8snMUc-_D63l4EQsHM",
+    authDomain: "final-project-b4001.firebaseapp.com",
+    databaseURL: "https://final-project-b4001.firebaseio.com",
+    storageBucket: "final-project-b4001.appspot.com",
+};
+
+  firebase.initializeApp(config);
+
+var newVideo;
+
+//Load viddeo click function
+$('#submit').click(function(event){
+  event.preventDefault();
+  
+  var newPlayerId = 'player' + playerCounter;
+  
+  
+  loadPlayer();
+  
+  function getVideoId() {
+    return $('#urlInput').val();
+  }
+
+  console.log(newPlayerId);
+
+
+  function loadPlayer() { 
+    if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+      console.log('undefined');
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+      window.onYouTubePlayerAPIReady = function() {
+        onYouTubePlayer();
+        console.log('byId');
+      };
+    } else {
+      console.log('is defined');
+    }
+  }
+
+  function onYouTubePlayer() {
+   
+    videosInFirebase.on('value', function(results) {
+
+    $('#video-feed').empty();
+
+      // $('.message-board').empty();
+
+      var videos = results.val();
+
+      // iterate through dom elements
+      //   id = id portion of dom id
+      //   if (id portion of dom id === videos)
+
+      var weirdIds = Object.keys(videos);
+
+      weirdIds.forEach(function(weirdId, index) {
+        var videoObject = videos[weirdId];
+        var videoId = videoObject.videoId;
+        var artist = videoObject.artist;
+        var song = videoObject.song;
+        var votes = videoObject.votes;
+        $('#video-feed').append('<div id=player-"' + weirdId + '">')
+
+        var player = new YT.Player('player-' + weirdId, {
+                height: '240',
+                width: '320',
+                videoId: weirdId,
+                events: {
+                    'onReady': onPlayerReady
+                }
+            })
+
+
+        var messageInFirebase = firebaseDB.ref('videos/' + weirdId);
+
+        var $votesSpan = $('<span>').addClass('votes').html(votes);
+
+       
+
+        $votesSpan.on('click', function() {
+          videosInFirebase.update({
+            votes: votes + 1
+          });
+        });
+
+    console.log('function' + newPlayerId);
+  }
+
+  $('.slider').slideUp(400);
+  // $('#urlInpu  t').val('');
+  $('#artistInput').val('');
+  $('#songInput').val('');
+  playerCounter += 1;
+}); //end video loader
+
+
+
+
+var firebaseDB = firebase.database();
+var videosInFirebase = firebaseDB.ref('urls');
+
+$('#submit').on('click', function(event) {
+  event.preventDefault();
+  var videoId = $('#urlInput').val();
+  var artistName = $('#artistInput').val();
+  var songName = $('#songInput').val();
+  console.log(videoId + artistName + songName);
+  
+
+  // "Create" a new message object in the database
+  videosInFirebase.push({
+    videoId: videoId,
+    artist: artistName,
+    song: songName,
+    votes: 0
+  });
+
+var onYouTubeIframeAPIReady;
+
+});
+});
+
+    // var $videoLi = $('<li>').html(video).append($votesSpan);
+    // $('.video-feed').append($videoLi);
+
--- js/app3.js
+++ js/app3.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    // loadPlayer();
 });
 
 // Input fields
@@ -10,112 +9,64 @@ $('#urlInput').click(function() {
 });
 
 // Initialize Firebase
-var config = {
+ var config = {
     apiKey: "AIzaSyD33LPylUvbdB7cq8snMUc-_D63l4EQsHM",
     authDomain: "final-project-b4001.firebaseapp.com",
     databaseURL: "https://final-project-b4001.firebaseio.com",
     storageBucket: "final-project-b4001.appspot.com",
   };
-
   firebase.initializeApp(config);
 
 var playerCounter = 1;
 var newVideo;
 var player;
 
-//Load viddeo click function
-$('#submit').click(function(event){
-  event.preventDefault();
-  // $('#video-feed').append('<div id="player">')
-  loadPlayer();
-  playerCounter += 1;
-
-  function getVideoId() {
-    console.log($('#urlInput').val())
-    return $('#urlInput').val();
-  }
-
-  console.log(newVideo);
-
-  // getVideoId();
-
-  function loadPlayer() { 
-    if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
-      console.log('undefined');
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onYouTubePlayer() {
+  videosInFirebase.on('value', function(results) {
+    console.log('we got value');
+    var videos = results.val();
+  
+    var weirdIds = Object.keys(videos);
+  
+    weirdIds.forEach(function(weirdId, index) {
+      console.log(weirdId);
+      var videoObject = videos[weirdId];
+      var url = videoObject.url;
+      var artist = videoObject.artist;
+      var song = videoObject.song;
+      var votes = videoObject.votes;
+      $('#video-feed').append('<div id=player' + weirdId + '>');
       
-      window.onYouTubePlayerAPIReady = function() {
-        onYouTubePlayer();
-        console.log('byId');
-      };
-    } else {
-      console.log('is defined');
-    }
-  }
-
-
-
-  function onYouTubePlayer() {
-    player = new YT.Player('player', {
-      height: '490',
-      width: '880',
-      videoId: getVideoId(),
-      playerVars: { controls:1, showinfo: 0, rel: 0, showsearch: 0, iv_load_policy: 3 },
-      events: {
-        'onStateChange': onPlayerStateChange,
-        'onError': catchError
-      }
-    });
-    console.log('player');
-  }
-  $('.slider').slideUp(400);
-  // $('#urlInput').val('');
-  $('#artistInput').val('');
-  $('#songInput').val('');
-}); //end video loader
-
-
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    // setTimeout(stopVideo, 6000);
-    done = true;
-  }
-  else if(event.data == YT.PlayerState.ENDED)
-  {
-    location.reload();
-  }
+      var player = new YT.Player('player' + weirdId, {
+          height: '490',
+          width: '880',
+          videoId: weirdId,
+          playerVars: { controls:1, showinfo: 0, rel: 0, showsearch: 0, iv_load_policy: 3 },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+      });   
+    }); //end video loader
+  });
 }
 
-function onPlayerReady(event) {
-
-  //if(typeof(SONG.getVideoId()) == undefined)
-  //{
-  //  console.log("undefineeeed"); 
-  //} 
-  //event.target.playVideo();   
+function loadPlayer() { 
+  if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+    console.log('undefined');
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    window.onYouTubePlayerAPIReady = function() {
+      console.log('API ready');
+      onYouTubePlayer();
+    };
+  } else {
+    console.log('is defined BAD');
+  }
 }
-
-function catchError(event)
-{
-  if(event.data == 100) console.log("De video bestaat niet meer");
-}
-
-function stopVideo() {
-  player.stopVideo();
-}
-
-// function PostVideo (url, artist, song) {
-//   firebase.database().ref('artists/' + artist).set({
-//     url: url,
-//     artist: artist,
-//     song: song,
-//     votes: 0
-//   });
-// }
 
 var firebaseDB = firebase.database();
 var videosInFirebase = firebaseDB.ref('urls');
@@ -137,37 +88,35 @@ $('#submit').on('click', function(event) {
   });
 });
 
-videosInFirebase.on('value', function(results) {
-  // $('.message-board').empty();
-  console.log(results);
+loadPlayer();
 
-  var videos = results.val();
+//Load viddeo click function
+$('#submit').click(function(event){
+  event.preventDefault();
+  // $('#video-feed').append('<div id="player">')
+  
+  playerCounter += 1;
 
-  var unsortedWeirdIds = Object.keys(videos);
-  var weirdIds = _.sortBy(unsortedWeirdIds, 'votes');
-
-  weirdIds.forEach(function(weirdId) {
-    var videoObject = videos[weirdId];
-    var url = videoObject.url;
-    var artist = videoObject.artist;
-    var song = videoObject.song;
-    var votes = videoObject.votes;
-
-
-    var messageInFirebase = firebaseDB.ref('videos/' + weirdId);
-
-    var $votesSpan = $('<span>').addClass('votes').html(votes);
-
-   
-
-    $votesSpan.on('click', function() {
-      videosInFirebase.update({
-        votes: votes + 1
-      });
-    });
-
-    // var $videoLi = $('<li>').html(video).append($votesSpan);
-    // $('.video-feed').append($videoLi);
-  });
+  function getVideoId() {
+    console.log($('#urlInput').val());
+    return $('#urlInput').val();
+  }
 });
 
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
